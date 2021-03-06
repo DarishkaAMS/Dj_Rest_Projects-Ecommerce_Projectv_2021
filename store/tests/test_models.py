d165d98dda997844b503afbba17f227f3a9a92f7
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-06 06:13:58 +0200

    Add test_models.py

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,1 +1,13 @@
 from django.test import TestCase
+
+from store.models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self) -> None:
+        self.data1 = Category.objects.create(name='Dummy', slug='dummy')
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
