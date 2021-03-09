2840723ad88f85de014766c405aa2f0f8de309de
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-09 06:12:07 +0200

    Update TestCategoryModel

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from store.models import Category, Product
 
 
-class TestCategoriesModel(TestCase):
+class TestCategoryModel(TestCase):
 
     def setUp(self) -> None:
         self.data1 = Category.objects.create(name='Dummy', slug='dummy')
@@ -11,3 +11,8 @@ class TestCategoriesModel(TestCase):
     def test_category_model_entry(self):
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+
+
+class TestProductModel(TestCase):
+
+    def setUp(self) -> None:
