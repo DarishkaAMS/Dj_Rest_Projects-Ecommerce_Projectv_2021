c94bb2308a3eb179031d6d5a1fd5e8428acc9d32
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-12 06:32:02 +0200

    Add category_list_view

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ app_name = 'store'
 urlpatterns = [
     path('', views.product_list_view, name='all_products'),
     path('item/<slug:slug>/', views.product_detail_view, name='product_detail'),
+    path('search/<slug:category_slug>/', views.category_list_view, name='category_list'),
 ]
--- store/views.py
+++ store/views.py
@@ -17,3 +17,9 @@ def category_dropdown_view(request):
     return {
         'categories': Category.objects.all()
     }
+
+
+def category_list_view(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/products/category.html', {'category': category}, {'products': products})
