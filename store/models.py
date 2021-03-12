8925c311b7bd772ae303cb4e0e9d12c06af39908
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-12 06:52:11 +0200

    Fe

--- store/models.py
+++ store/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -11,7 +16,7 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
     def get_absolute_url(self):
-        return reverse('store:product_detail', args=[self.slug])
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -19,10 +24,10 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, related_name='product_category', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
-    description = models.TextField(default='Amazing description is coming')
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -30,9 +35,11 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
-        verbose_name_plural = 'products'
+        verbose_name_plural = 'Products'
         ordering = ('-created',)
 
     def get_absolute_url(self):
